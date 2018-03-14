6fd51f327120d7f076fd0b568d52bf9a6ef6832d
nevillco <connor.neville@raizlabs.com>
2018-03-14 13:12:34 -0400

    Add subdirectories and root view controller

--- SettleUp/App/AppDelegate.swift
+++ SettleUp/App/AppDelegate.swift
@@ -14,12 +14,20 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        configureWindow()
+        return true
+    }
+
+}
+
+private extension AppDelegate {
+
+    func configureWindow() {
         let window = UIWindow()
         window.backgroundColor = .white
-        window.rootViewController = UIViewController()
+        window.rootViewController = RootViewController()
         window.makeKeyAndVisible()
         self.window = window
-        return true
     }
 
 }
---
+++ SettleUp/Screens/RootViewController.swift
@@ -0,0 +1,13 @@
+//
+//  RootViewController.swift
+//  SettleUp
+//
+//  Created by Connor Neville on 3/14/18.
+//  Copyright © 2018 Connor Neville. All rights reserved.
+//
+
+import UIKit
+
+final class RootViewController: UIViewController {
+
+}
