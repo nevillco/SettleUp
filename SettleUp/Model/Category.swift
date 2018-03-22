f9f02d71258dbd38df3d2e35f0e0a14d2098093d
nevillco <connor.neville@raizlabs.com>
2018-03-22 14:10:30 -0400

    Add Rule and Category models

---
+++ SettleUp/Model/Category.swift
@@ -0,0 +1,18 @@
+//
+//  Category.swift
+//  SettleUp
+//
+//  Created by Connor Neville on 3/22/18.
+//  Copyright © 2018 Connor Neville. All rights reserved.
+//
+
+import Foundation
+
+struct Category: Codable {
+
+    let id: String
+    let title: String
+    let description: String
+    let ruleIDs: [String]
+
+}
---
+++ SettleUp/Model/Rule.swift
@@ -0,0 +1,19 @@
+//
+//  Rule.swift
+//  SettleUp
+//
+//  Created by Connor Neville on 3/22/18.
+//  Copyright © 2018 Connor Neville. All rights reserved.
+//
+
+import Foundation
+
+struct Rule: Codable {
+
+    let id: String
+    let description: String
+    let detailedDescription: String?
+    let exclusiveRuleIDs: [String]
+    let categoryID: String
+
+}
